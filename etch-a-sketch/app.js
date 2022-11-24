5ac0d61dc7184bf5e79568e9599339742f046fb7
Christopher Kim <christophminkim@gmail.com>
2022-11-24 01:16:21 -0800

    create 16x16 of square divs with hover effect

--- etch-a-sketch/app.js
+++ etch-a-sketch/app.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const squareContainer = document.querySelector('.square-container');
 
-  for (let i = 0; i < 256; i++) {
+  for (let i = 1; i <= 256; i++) {
     const square = document.createElement('div');
     square.className = 'square';
-    square.id = `square-${i + 1}`;
+    square.id = `square-${i}`;
     squareContainer.appendChild(square);
   }
 });
