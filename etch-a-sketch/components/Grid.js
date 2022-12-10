15ed24636359be88d2dde63f45855248ad82fb6a
Christopher Kim <christophminkim@gmail.com>
2022-12-09 23:53:02 -0800

    add createGrid method and export Grid

--- etch-a-sketch/components/Grid.js
+++ etch-a-sketch/components/Grid.js
@@ -2,4 +2,15 @@ class Grid {
   constructor(size) {
     this.size = size;
   }
+
+  createGrid = (size) => {
+    const grid = document.querySelector('.grid');
+    for (let i = 0; i < size * size; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      grid.appendChild(square);
+    }
+  };
 }
+
+export default Grid;
