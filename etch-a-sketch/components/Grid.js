df7f933cf21f0236a0ac5c33ba2766db8977d4f1
Christopher Kim <christophminkim@gmail.com>
2022-12-11 19:03:32 -0800

    add logic for dynamic squares with constant grid size

--- etch-a-sketch/components/Grid.js
+++ etch-a-sketch/components/Grid.js
@@ -5,6 +5,9 @@ class Grid {
 
   createGrid = (size) => {
     const grid = document.querySelector('.grid');
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
     for (let i = 0; i < size * size; i++) {
       const square = document.createElement('div');
       square.classList.add('square');
