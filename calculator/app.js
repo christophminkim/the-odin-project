ca078df888e04e87c7d0f054552d9449be474223
Christopher Kim <christophminkim@gmail.com>
2022-12-18 21:49:35 -0800

    Import Calculator and log all inputs

--- calculator/app.js
+++ calculator/app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+import Calculator from './components/Calculator.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const calc = new Calculator();
+  calc.displayInput();
+});
